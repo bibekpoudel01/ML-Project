9fb15d235366d51be04eaed52e7ac9d4fa6acd7f
bibekpoudel01 <bibekpoudel314@gmail.com>
2025-11-18 11:12:15 +0545

    Hyperparameter Tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -8,15 +8,15 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
-from ..exception import CustomException
-from ..logger import logging
+from src.exception import CustomException
+from src.logger import logging
 import os
 
-from ..utils import save_object
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts', "preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -114,6 +114,7 @@ class DataTransformation:
                 obj=preprocessing_obj
 
             )
+
             return (
                 train_arr,
                 test_arr,
